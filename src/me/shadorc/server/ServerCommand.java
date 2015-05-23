a5f4aed0e5e14ff5855fc92ffd075ca0c6639614
Shadorc <bi-crosseur@hotmail.fr>
2015-05-23 23:04:13 +0200

    Remove Server /total command

--- src/me/shadorc/server/ServerCommand.java
+++ src/me/shadorc/server/ServerCommand.java
@@ -7,15 +7,11 @@ public class ServerCommand {
 
 	public static void admin(String command) {
 		switch(command.toLowerCase()) {
-			case "/total":
-				ServerFrame.dispMessage("Nombre de connectés : " + Server.getClients().size());
-				return;
 			case "/ip":
 				ServerFrame.dispMessage("IP : " + Server.getIp());
 				Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(Server.getIp()), null);
 				return;
 			case "/help":
-				ServerFrame.dispMessage("Nombre de connectés : /total");
 				ServerFrame.dispMessage("IP du serveur : /ip");
 				return;
 			default:
